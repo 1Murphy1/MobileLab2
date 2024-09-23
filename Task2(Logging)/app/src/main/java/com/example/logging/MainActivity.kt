3e41b0c808d29a73541d069db332f0af5bd2775a
1Murphy1 <115133958+1Murphy1@users.noreply.github.com>
2024-09-23 22:42:07 +0700

    Task2

---
+++ Task2(Logging)/app/src/main/java/com/example/logging/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.logging
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.widget.EditText
+import android.widget.Button
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val editText: EditText = findViewById(R.id.editText)
+        val buttonLog: Button = findViewById(R.id.button_log)
+        val buttonTimber = findViewById<Button>(R.id.button_timber)
+
+        Timber.plant(Timber.DebugTree())
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        buttonLog.setOnClickListener {
+            val text = editText.text.toString()
+
+            Log.v("From EditText", text)
+        }
+
+        buttonTimber.setOnClickListener {
+            val text = editText.text.toString()
+
+            Timber.v(text)
+        }
+    }
+}
