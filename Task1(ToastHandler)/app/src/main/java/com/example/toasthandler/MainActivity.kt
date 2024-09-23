e8b401a54057bd914be260faedaa0a87a90d0ce5
1Murphy1 <115133958+1Murphy1@users.noreply.github.com>
2024-09-23 22:39:32 +0700

    Task1

---
+++ Task1(ToastHandler)/app/src/main/java/com/example/toasthandler/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.toasthandler
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val button: Button = findViewById(R.id.button_ok)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Кнопка ОК", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
